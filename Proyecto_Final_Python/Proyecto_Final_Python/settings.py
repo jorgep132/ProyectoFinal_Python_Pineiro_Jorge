7b010a5e6f6abe53049748bcb03740a53b0b8c3f
Jorge Piñeiro <146791685+jorgep132@users.noreply.github.com>
2024-01-16 14:50:26 +0000

    Probando index

--- Proyecto_Final_Python/Proyecto_Final_Python/settings.py
+++ Proyecto_Final_Python/Proyecto_Final_Python/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for Proyecto_Final_Python project.
 
@@ -54,7 +56,7 @@ ROOT_URLCONF = "Proyecto_Final_Python.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [os.path.join(BASE_DIR, 'Proyecto_Final_Python_App/templates')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -115,8 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
-
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'Proyecto_Final_Python_App/static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
--- Proyecto_Final_Python/Proyecto_Final_Python/urls.py
+++ Proyecto_Final_Python/Proyecto_Final_Python/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Proyecto_Final_Python_App import views
+
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.index, name='index'),
 ]
