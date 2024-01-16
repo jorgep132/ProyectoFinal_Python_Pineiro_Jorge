ce63e63b7242fbc4642ff2dbd76428b067ab240f
Jorge Piñeiro <146791685+jorgep132@users.noreply.github.com>
2024-01-16 14:26:30 +0000

    Commit 16/01

---
+++ Proyecto_Final_Python/Proyecto_Final_Python_App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ProyectoFinalPythonAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "Proyecto_Final_Python_App"
