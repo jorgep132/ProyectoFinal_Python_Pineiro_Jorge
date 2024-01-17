1a347fceafd635228045990e16a3c93d63173a5c
Jorge Piñeiro <jorge.p.salvagno@gmail.com>
2024-01-17 09:50:12 -0300

    Proyecto Final Python

--- Proyecto_Final_Python/Proyecto_Final_Python_App/models.py
+++ Proyecto_Final_Python/Proyecto_Final_Python_App/models.py
@@ -9,6 +9,9 @@ class Juegos(models.Model):
     title = models.CharField(max_length=255)
     image = models.ImageField(upload_to='product_images/')
     url = models.CharField(max_length=255, default='/default-url/')
+    genero1 = models.CharField(max_length=32, default='')
+    genero2 = models.CharField(max_length=32, default='')
+    genero3 = models.CharField(max_length=32, default='')
 
     def __str__(self):
         return self.title
