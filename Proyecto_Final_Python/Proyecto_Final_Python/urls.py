75eb6e44c293d6242d8952e3b94889321c440ed8
Jorge Piñeiro <jorge.p.salvagno@gmail.com>
2024-01-21 14:49:52 -0300

    Falta poco

--- Proyecto_Final_Python/Proyecto_Final_Python/urls.py
+++ Proyecto_Final_Python/Proyecto_Final_Python/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import LogoutView
@@ -20,3 +22,5 @@ urlpatterns = [
 ]
  
 
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
